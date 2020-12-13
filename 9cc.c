2d8bb63bd531ae095cddd9c4c2bed2a3ce0b8475
HoP-adamas <bh.evaporation6174@gmail.com>
2020-12-13 22:31:59 +0900

    add function error_at()

--- 9cc.c
+++ 9cc.c
@@ -3,6 +3,22 @@
 // the token we focus on
 Token *token;
 
+char *user_input;
+
+// Reports an error location and exit.
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^");
+    vprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 int main(int argc, char **argv) {
 
     if (argc != 2) {
